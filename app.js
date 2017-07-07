31414c5816a97609c7fbd38ad749a80663b15c3f
kanubiz <duartejhfaria@gmail.com>
2017-07-08 00:22:41 +0100

    Intro to Components

--- app.js
+++ app.js
@@ -1,12 +1,20 @@
-new Vue({
-  el: 'vue-app'
-  data: {
-
+Vue.component('greeting', {
+  template: '<p>Hey there, I am {{name}}. <button @click="changeName()">Change Name</button></p>',
+  data: function() {
+    return {
+      name: 'Yoshi'
+    }
   },
   methods: {
-
-  },
-  computed: {
-
+    changeName: function() {
+      this.name = 'Mario';
+    }
   }
+});
+
+new Vue({
+  el: '#vue-app-one'
+});
+new Vue({
+  el: '#vue-app-two'
 });
